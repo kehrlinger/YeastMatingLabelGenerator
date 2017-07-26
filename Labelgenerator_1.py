9b60a3b07438b171b62b7ad7785d2d49b6ec3d74
organhaendler <k.ehrlinger@web.de>
2017-07-26 02:34:15 +0200

    Update Labelgenerator_1.py
    The labels are now not printed in a single row. Instead after every 20 labels, the next column is chosen. This can be done until column 'Z' (~520 labels possible).
    
    The existence of a target worksheet is now checked.
    If it exists, a new sheet is generated, if it does not exist it is generated.
    
    Sheets are now labelled with date.

--- Labelgenerator_1.py
+++ Labelgenerator_1.py
@@ -2,6 +2,9 @@
 # combines them in the same way a mating assay would be carried out.
 # An excel spreadsheet is created and the generated labels are saved to this spreadsheet
 import openpyxl
+import string
+import datetime
+import os.path
 
 vec1 = input('Please insert the first vector: \n(for example: pGADT7)\n')
 ins_vec_1 = input ('Please enter the inserts of this vector, delimited by a whitespace: \n'
@@ -30,12 +33,38 @@ for v in final_ins_vec_1:
 
 # Construct a list with coordinates
 coordinates =[]
+letters = list(string.ascii_uppercase)
+letter_index = 0
+loopcount_coordinates = 0
 for c in range(0, len(labels)):
-    coordinate = 'A' + str(c+1)
-    coordinates.append(coordinate)
+    #print (loopcount_coordinates)
+    if loopcount_coordinates < 15:
+        loopcount_coordinates += 1
+        coordinate = letters[letter_index] + str(loopcount_coordinates)
+        coordinates.append(coordinate)
+    else:
+        loopcount_coordinates = 1
+        letter_index += 1
+        coordinate = letters[letter_index] + str(loopcount_coordinates)
+        coordinates.append(coordinate)
 
-wb = openpyxl.load_workbook('MatingLabels.xlsx')
-sheet = wb.create_sheet(index=0, title='Labels')
-for index in range(0, len(labels)):
-    sheet[coordinates[index]] = labels[index]
-wb.save('MatingLabels.xlsx')
+# Write everything to a workbook
+# Check if the workbook TubeLabels.xlsx already exists, proceed accordingly
+# WB is saved to Tubelabels.xlsx, if it exists a new sheet is generated
+# Sheetname is 'Date Labels'
+if os.path.isfile('./TubeLabels.xlsx'):
+    wb = openpyxl.load_workbook('TubeLabels.xlsx')
+    timestamp = datetime.date.today()
+    sheet = wb.create_sheet(index=0, title= str(timestamp) + ' Labels')
+    for index in range(0, len(labels)):
+        sheet[coordinates[index]] = labels[index]
+    wb.save('TubeLabels.xlsx')
+else:
+    timestamp = datetime.date.today()
+    # Create a new workbook. I still don't fully understand openpyxl
+    wb = openpyxl.workbook.Workbook()
+    sheet = wb.active
+    sheet.title = str(timestamp) + ' Labels'
+    for index in range(0, len(labels)):
+        sheet[coordinates[index]] = labels[index]
+    wb.save('TubeLabels.xlsx')
